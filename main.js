57adecc784c0a87566a8c56453d622fd4a8fbd81
Chad Pjontek <chadpjontek@gmail.com>
2017-11-23 04:18:14 -0500

    Multiply/divide functions added. Some error handling. More to be done. Must sleep for turkey day.

--- main.js
+++ main.js
@@ -21,62 +21,121 @@ $("button").click(function () {
       }
       $("#current-output").text(curOutput);
     }
+    // too long err message
+  } else if (curOutput.length > 10 && (Number(buttonPressed) || buttonPressed === "0")) {
+    limitError();
     // clear all
   } else if (buttonPressed === "c") {
+    clear();
+    // add
+  } else if (buttonPressed === "add") {
+    if (isOperating) {
+      chaOutput = chaOutput.replace(/ [÷X+-] $/, " + ");
+      $("#chained-output").text(chaOutput);
+    } else {
+      if (chaOutput === "0") {
+        chaOutput = curOutput + " + ";
+        total = Number(curOutput);
+      } else {
+        chaOutput += curOutput + " + ";
+        total += Number(curOutput);
+        if (total.toString().length > 10) {
+          // too long message
+          limitError();
+        }
+        curOutput = total.toString();
+        $("#current-output").text(curOutput);
+      }
+      isOperating = true;
+      if (chaOutput.length > 22) {
+        var temp = chaOutput.split('');
+        temp.splice(0, chaOutput.length - 22);
+        chaOutput = temp.join('');
+      }
+      $("#chained-output").text(chaOutput);
+    }
+    // subtract
+  } else if (buttonPressed === "subtract") {
+    if (isOperating) {
+      chaOutput = chaOutput.replace(/ [÷X+-] $/, " - ");
+      $("#chained-output").text(chaOutput);
+    } else {
+      if (chaOutput === "0") {
+        chaOutput = curOutput + " - ";
+        total = Number(curOutput);
+      } else {
+        chaOutput += curOutput + " - ";
+        total -= Number(curOutput);
+        curOutput = total.toString();
+        $("#current-output").text(curOutput);
+      }
+      isOperating = true;
+      if (chaOutput.length > 22) {
+        var temp = chaOutput.split('');
+        temp.splice(0, chaOutput.length - 22);
+        chaOutput = temp.join('');
+      }
+      $("#chained-output").text(chaOutput);
+    }
+    // multiply
+  } else if (buttonPressed === "multiply") {
+    if (isOperating) {
+      chaOutput = chaOutput.replace(/ [÷X+-] $/, " X ");
+      $("#chained-output").text(chaOutput);
+    } else {
+      if (chaOutput === "0") {
+        chaOutput = curOutput + " X ";
+        total = Number(curOutput);
+      } else {
+        chaOutput += curOutput + " X ";
+        total *= Number(curOutput);
+        curOutput = total.toString();
+        $("#current-output").text(curOutput);
+      }
+      isOperating = true;
+      if (chaOutput.length > 22) {
+        var temp = chaOutput.split('');
+        temp.splice(0, chaOutput.length - 22);
+        chaOutput = temp.join('');
+      }
+      $("#chained-output").text(chaOutput);
+    }
+    // divide
+  } else if (buttonPressed === "divide") {
+    if (isOperating) {
+      chaOutput = chaOutput.replace(/ [÷X+-] $/, " ÷ ");
+      $("#chained-output").text(chaOutput);
+    } else {
+      if (chaOutput === "0") {
+        chaOutput = curOutput + " ÷ ";
+        total = Number(curOutput);
+      } else {
+        chaOutput += curOutput + " ÷ ";
+        total /= Number(curOutput);
+        curOutput = total.toString();
+        $("#current-output").text(curOutput);
+      }
+      isOperating = true;
+      if (chaOutput.length > 22) {
+        var temp = chaOutput.split('');
+        temp.splice(0, chaOutput.length - 22);
+        chaOutput = temp.join('');
+      }
+      $("#chained-output").text(chaOutput);
+    }
+  }
+  function limitError(){
+    $("#current-output").text("digit limit");
+    curOutput = "0";
+    chaOutput = "0"
+    total = 0;
+    $("#chained-output").text(chaOutput);
+  }
+  function clear(){
     curOutput = "0"
     chaOutput = "0"
     total = 0;
     $("#current-output").text(curOutput);
     $("#chained-output").text(chaOutput);
-    // add
-  } else if (buttonPressed === "add") {
-    if (curOutput !== "0") {
-      if (isOperating) {
-        chaOutput = chaOutput.replace(/ [÷X+-] $/, " + ");
-        $("#chained-output").text(chaOutput);
-      } else {
-        if (chaOutput === "0") {
-          chaOutput = curOutput + " + ";
-          total = Number(curOutput);
-        } else {
-          chaOutput += curOutput + " + ";
-          total += Number(curOutput);
-          curOutput = total.toString();
-          $("#current-output").text(curOutput);
-        }
-        isOperating = true;
-        if(chaOutput.length > 22) {
-          var temp = chaOutput.split('');
-          temp.splice(0, chaOutput.length - 22);
-          chaOutput = temp.join('');
-        }
-        $("#chained-output").text(chaOutput);
-      }
-    }
-    // subtract
-  } else if (buttonPressed === "subtract") {
-    if (curOutput !== "0") {
-      if (isOperating) {
-        chaOutput = chaOutput.replace(/ [÷X+-] $/, " - ");
-        $("#chained-output").text(chaOutput);
-      } else {
-        if (chaOutput === "0") {
-          chaOutput = curOutput + " - ";
-          total = Number(curOutput);
-        } else {
-          chaOutput += curOutput + " - ";
-          total -= Number(curOutput);
-          curOutput = total.toString();
-          $("#current-output").text(curOutput);
-        }
-        isOperating = true;
-        if(chaOutput.length > 22) {
-          var temp = chaOutput.split('');
-          temp.splice(0, chaOutput.length - 22);
-          chaOutput = temp.join('');
-        }
-        $("#chained-output").text(chaOutput);
-      }
-    }
   }
 });
