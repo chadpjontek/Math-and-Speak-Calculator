8505a385d30c4780f7aec73d44ab01ce78884fef
Chad Pjontek <chadpjontek@gmail.com>
2017-11-24 04:56:11 -0500

    Equals and decimal point functions added. Reworked code, fixed errors, and organized functions.

--- main.js
+++ main.js
@@ -1,141 +1,120 @@
-// variable used to store current and chained output, the running total, if there is a decimal, and if +, -, X, or ÷ has been just activated
+// vars
 var curOutput = "0";
-var chaOutput = "0";
+var chainOutput = "";
+var inputArr = [];
 var total = 0;
-var isOperating = false;
-var isDecimal = false;
-// handle any button press
-$("button").click(function () {
-  var buttonPressed = this.id;
-  // if a number is pressed and it's not too long for the UI update the UI
-  if (curOutput.length < 11 && (Number(buttonPressed) || buttonPressed === "0")) {
-    if (isOperating) {
-      curOutput = buttonPressed;
-      isOperating = false;
-      $("#current-output").text(curOutput);
-    } else {
-      if (curOutput === "0") {
-        curOutput = buttonPressed;
-      } else {
-        curOutput += buttonPressed;
-      }
-      $("#current-output").text(curOutput);
-    }
-    // too long err message
-  } else if (curOutput.length > 10 && (Number(buttonPressed) || buttonPressed === "0")) {
+var decimalActive = false;
+var buttonPressed;
+var operators = ["+", "-", "*", "/"];
+var operatorIds = ["add", "subtract", "multiply", "divide"];
+var equalActivated = false;
+// functions
+function displayCurOutput() {
+  if (curOutput.length > 10) {
     limitError();
-    // clear all
+  } else {
+    $("#current-output").text(curOutput);
+  }
+}
+function limitError() {
+  curOutput = "0";
+  chainOutput = "";
+  total = 0;
+  inputArr = [];
+  $("#current-output").text("digit limit");
+  $("#chained-output").text(chainOutput);
+  decimalActive = false;
+  equalActivated = false;
+}
+function clear() {
+  curOutput = "0";
+  chainOutput = "";
+  total = 0;
+  inputArr = [];
+  $("#current-output").text(curOutput);
+  $("#chained-output").text(chainOutput);
+  decimalActive = false;
+  equalActivated = false;
+}
+function displayChainOutput() {
+  chainOutput = inputArr.join('');
+  chainOutput = chainOutput.replace(/([+])|([\-])|([/])|([*])/g, function (match, p1, p2, p3, p4) {
+    if (p1) { return " + "; }
+    if (p2) { return " - "; }
+    if (p3) { return " ÷ "; }
+    if (p4) { return " X "; }
+  });
+  chainOutput = chainOutput.substr(-22);
+  $("#chained-output").text(chainOutput);
+}
+function getTotal() {
+  total = Math.round(eval(inputArr.join('')) * 10000) / 10000;
+}
+function addDecimal() {
+  inputArr.push(".");
+  displayCurOutput();
+  decimalActive = true;
+}
+// handle button presses
+$("button").click(function () {
+  buttonPressed = this.id;
+  // numbers
+  if (Number(buttonPressed) || buttonPressed === "0") {
+    if (equalActivated) {
+      clear();
+      equalActivated = false;
+    }
+    if (operators.includes(inputArr[inputArr.length - 1]) || curOutput === "0") {
+      curOutput = buttonPressed;
+    } else {
+      curOutput += buttonPressed;
+    }
+    inputArr.push(buttonPressed);
+    displayCurOutput();
+    // clear
   } else if (buttonPressed === "c") {
     clear();
-    // add
-  } else if (buttonPressed === "add") {
-    if (isOperating) {
-      chaOutput = chaOutput.replace(/ [÷X+-] $/, " + ");
-      $("#chained-output").text(chaOutput);
-    } else {
-      if (chaOutput === "0") {
-        chaOutput = curOutput + " + ";
-        total = Number(curOutput);
-      } else {
-        chaOutput += curOutput + " + ";
-        total += Number(curOutput);
-        if (total.toString().length > 10) {
-          // too long message
-          limitError();
-        }
-        curOutput = total.toString();
-        $("#current-output").text(curOutput);
-      }
-      isOperating = true;
-      if (chaOutput.length > 22) {
-        var temp = chaOutput.split('');
-        temp.splice(0, chaOutput.length - 22);
-        chaOutput = temp.join('');
-      }
-      $("#chained-output").text(chaOutput);
+    // operators
+  } else if (operatorIds.includes(buttonPressed)) {
+    decimalActive = false;
+    if (equalActivated) {
+      equalActivated = false;
     }
-    // subtract
-  } else if (buttonPressed === "subtract") {
-    if (isOperating) {
-      chaOutput = chaOutput.replace(/ [÷X+-] $/, " - ");
-      $("#chained-output").text(chaOutput);
-    } else {
-      if (chaOutput === "0") {
-        chaOutput = curOutput + " - ";
-        total = Number(curOutput);
-      } else {
-        chaOutput += curOutput + " - ";
-        total -= Number(curOutput);
-        curOutput = total.toString();
-        $("#current-output").text(curOutput);
-      }
-      isOperating = true;
-      if (chaOutput.length > 22) {
-        var temp = chaOutput.split('');
-        temp.splice(0, chaOutput.length - 22);
-        chaOutput = temp.join('');
-      }
-      $("#chained-output").text(chaOutput);
+    if (operators.includes(inputArr[inputArr.length - 1])) {
+      inputArr.pop();
+      inputArr.push(operators[operatorIds.indexOf(buttonPressed)]);
+      displayChainOutput();
+    } else if (inputArr.length === 0) {
+      inputArr.push("0", operators[operatorIds.indexOf(buttonPressed)]);
+      displayChainOutput();
+    } else if (inputArr[inputArr.length - 1] !== ".") {
+      getTotal();
+      inputArr.push(operators[operatorIds.indexOf(buttonPressed)]);
+      displayChainOutput();
+      curOutput = total.toString();
+      displayCurOutput();
     }
-    // multiply
-  } else if (buttonPressed === "multiply") {
-    if (isOperating) {
-      chaOutput = chaOutput.replace(/ [÷X+-] $/, " X ");
-      $("#chained-output").text(chaOutput);
-    } else {
-      if (chaOutput === "0") {
-        chaOutput = curOutput + " X ";
-        total = Number(curOutput);
-      } else {
-        chaOutput += curOutput + " X ";
-        total *= Number(curOutput);
-        curOutput = total.toString();
-        $("#current-output").text(curOutput);
-      }
-      isOperating = true;
-      if (chaOutput.length > 22) {
-        var temp = chaOutput.split('');
-        temp.splice(0, chaOutput.length - 22);
-        chaOutput = temp.join('');
-      }
-      $("#chained-output").text(chaOutput);
+    // equals
+  } else if (buttonPressed === "equals") {
+    if (inputArr.length > 0 && !operators.includes(inputArr[inputArr.length - 1]) && inputArr[inputArr.length - 1] !== ".") {
+      getTotal();
+      chainOutput += curOutput + " = " + total.toString();
+      chainOutput = chainOutput.substr(-22);
+      $("#chained-output").text(chainOutput);
+      curOutput = total.toString();
+      displayCurOutput();
+      inputArr = [curOutput];
+      equalActivated = true;
+      decimalActive = true;
     }
-    // divide
-  } else if (buttonPressed === "divide") {
-    if (isOperating) {
-      chaOutput = chaOutput.replace(/ [÷X+-] $/, " ÷ ");
-      $("#chained-output").text(chaOutput);
-    } else {
-      if (chaOutput === "0") {
-        chaOutput = curOutput + " ÷ ";
-        total = Number(curOutput);
-      } else {
-        chaOutput += curOutput + " ÷ ";
-        total /= Number(curOutput);
-        curOutput = total.toString();
-        $("#current-output").text(curOutput);
-      }
-      isOperating = true;
-      if (chaOutput.length > 22) {
-        var temp = chaOutput.split('');
-        temp.splice(0, chaOutput.length - 22);
-        chaOutput = temp.join('');
-      }
-      $("#chained-output").text(chaOutput);
+    // decimal point
+  } else if (buttonPressed === "point") {
+    if (!decimalActive && !operators.includes(inputArr[inputArr.length - 1])) {
+      curOutput += ".";
+      addDecimal();
+    } else if (!decimalActive && operators.includes(inputArr[inputArr.length - 1])) {
+      curOutput = ".";
+      addDecimal();
     }
-  }
-  function limitError(){
-    $("#current-output").text("digit limit");
-    curOutput = "0";
-    chaOutput = "0"
-    total = 0;
-    $("#chained-output").text(chaOutput);
-  }
-  function clear(){
-    curOutput = "0"
-    chaOutput = "0"
-    total = 0;
-    $("#current-output").text(curOutput);
-    $("#chained-output").text(chaOutput);
   }
 });
